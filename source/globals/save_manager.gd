d9337f266016833a33722235445bed919df65bec
hebedebe <hebedebe1@gmail.com>
2026-09-26 23:02:02 +1000

    players can now reset saves

--- source/globals/save_manager.gd
+++ source/globals/save_manager.gd
@@ -4,6 +4,11 @@ const SAVE_PATH: String = "user://savegame.save"
 
 var event_listener: EventTrigger
 
+var auto_load: bool = true
+
+var autosave: bool = true
+var autosave_interval: float = 0.5
+
 func _ready() -> void:
 	event_listener = EventTrigger.new()
 	add_child(event_listener)
@@ -11,11 +16,25 @@ func _ready() -> void:
 	event_listener.add_callback(&"load_game", "load_game")
 	
 	print("Save manager initialised")
+	
+	if auto_load:
+		await get_tree().create_timer(0.2).timeout
+		load_game()
+	autosave_loop()
+
+func autosave_loop() -> void:
+	await get_tree().create_timer(autosave_interval).timeout
+	if autosave:
+		save_game()
+	autosave_loop()
 
 func save_game() -> void:
-	print("Starting DOT save")
+	#print("Starting DOT save")
 	DOT_save.save_data(0.1)
 	
 func load_game() -> void:
-	print("Starting DOT load")
+	#print("Starting DOT load")
 	DOT_save.load_data()
+
+func clear_save() -> void:
+	DOT_save.delete_data()
